12c4fe669d130ee515e5b45472b3b28dfcbad9bb
mahamatahmat23 <49762748+mahamatahmat23@users.noreply.github.com>
2019-10-25 00:43:43 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+const form= document.querySelector('form');
+const name= document.querySelector('#name');
+const cost= document.querySelector('#cost');
+const error= document.querySelector('#error');
+
+form.addEventListener('submit',(e) => {
+	
+	e.preventDefault();
+	if(name.value && cost.value){
+		const item={
+			name:name.value,
+			cost:parseInt(cost.value)
+		};
+		db.collection('expenses').add(item).then(res=>{
+			error.textContent="";
+			name.value="";
+			cost.value="";
+
+		})
+
+	} else{
+		error.textContent="please enter values before submitting";
+	}
+} );
+function myfunction(foo) {
+    var elem = foo.getElementsByTagName('p');
+    var TextInsideLi = elem[0].innerHTML;
+
+  };
+
